3cc976dbbf3fb8bf42d069f83ccd9bcb8ed78f5b
Harlock221B <gabrielferreira221b@gmail.com>
2023-06-27 11:35:09 -0300

    Drag and Drop Stduy

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <header class="header"></header>
+</template>
+
+<script>
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'Header'
+}
+</script>
+
+<style scoped>
+.header{
+    width: 100%;
+    height: 4.5rem;
+    background: var(--color-green);
+}
+</style>
